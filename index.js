864d429049ba63da7e82ab1bd33c72dff7ab13e9
FredericVL <frederic.van.linthoudt@gmail.com>
2018-01-20 18:35:40 +0100

    Fix for next schedule time (make sure time is 24h format)

--- index.js
+++ index.js
@@ -264,7 +264,7 @@ EvohomeThermostatAccessory.prototype = {
                     weekday[5]="Friday";
                     weekday[6]="Saturday";
                     
-                    var currenttime = correctDate.toLocaleTimeString();
+                    var currenttime = correctDate.toLocaleTimeString('nl-BE');
                     var proceed = true;
                     var nextScheduleTime = "";
                     
