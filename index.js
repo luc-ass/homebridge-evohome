c1b093f90f94a05a81f3ab12a3955500be96673b
Lucas Gasenzer <lucasgasenzer@mac.com>
2021-01-19 14:55:45 +0100

    reformatting for better readability

--- index.js
+++ index.js
@@ -131,7 +131,9 @@ EvohomePlatform.prototype = {
                                         // timezone offset in minutes
                                         var offsetMinutes = locations[that.locationIndex].timeZone.offsetMinutes;
                                         // create accessory (only if it is "HeatingZone")
-                                        if (device.modelType == "HeatingZone" || device.modelType == "RoundWireless" || device.modelType == "RoundModulation") {
+                                        if (device.modelType == "HeatingZone" || 
+                                            device.modelType == "RoundWireless" || 
+                                            device.modelType == "RoundModulation") {
                                             var accessory = new EvohomeThermostatAccessory(that, that.log, name, device, locations[that.locationIndex].systemId, deviceId, thermostat, this.temperatureUnit, this.username, this.password, this.interval_setTemperature, offsetMinutes);
                                             // store accessory in myAccessories
                                             this.myAccessories.push(accessory);
