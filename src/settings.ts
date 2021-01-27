55379215d3ceda68b9570c0c5bc689ed99a53cdc
Lucas Gasenzer <lucasgasenzer@mac.com>
2021-01-27 21:02:16 +0100

    example settings.ts

---
+++ src/settings.ts
@@ -0,0 +1,35 @@
+
+// THIS HAS BEEN COPIED FROM homebridge-honeywell-home
+// IT IS AN EXAMPLE OF HOW TO STRUCTURE SUCH A FILE
+// THE URLs ARE NOT FOR EVOHOME!
+
+
+/**
+ * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
+ */
+export const PLATFORM_NAME = 'Evohome';
+
+/**
+ * This must match the name of your plugin as defined the package.json
+ */
+export const PLUGIN_NAME = 'homebridge-evohome';
+
+/**
+ * This is the main url used to access honeywell API
+ */
+//export const AuthURL = 'https://api.honeywell.com/oauth2/token';
+
+/**
+ * This is the main url used to access honeywell API
+ */
+//export const LocationURL = 'https://api.honeywell.com/v2/locations';
+
+/**
+ * This is the main url used to access honeywell API
+ */
+//export const DeviceURL = 'https://api.honeywell.com/v2/devices';
+
+/**
+ * This is the url used to access UI Login to honeywell API
+ */
+//export const UIurl = 'https://homebridge-honeywell.iot.oz.nu/user/refresh';
